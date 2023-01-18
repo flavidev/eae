f04fba13af5867886998c1279498ea7899de0861
Flavio Vieira <flavidev@Flavios-Air.lan>
2023-01-17 23:43:04 -0300

    background and centered logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/images/eae-logo-1.png";
+import background from "./assets/images/background.png";
+import "./App.css";
 
 function App() {
   return (
-   <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+    <div className="App">
+      <div style={styles.container}>
+        <img src={logo} style={styles.logo} alt="logo" />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const styles = {
+  container: {
+    backgroundImage: `url(${background})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    height: "100vh",
+    width: "100vw",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    height: "100px",
+    width: "100px",
+  },
+};
